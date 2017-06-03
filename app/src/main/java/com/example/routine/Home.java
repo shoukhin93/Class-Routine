d491d27addcadcdef4456507ec1ef5571909d698
Shoukhin <shoukhin1993@gmail.com>
2017-06-03 20:58:00 +0600

    spinner added in home

--- app/src/main/java/com/example/routine/Home.java
+++ app/src/main/java/com/example/routine/Home.java
@@ -2,12 +2,24 @@ package com.example.routine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Home extends AppCompatActivity {
+
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.semesters, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
     }
 }
